740b11bcec86a68970a27dbbf26ee043519b75c6
Gerson Diniz <gerson.diniz@sata.pt>
2021-11-20 16:40:59 -0100

    Added devtools

--- template/src/store/store.js
+++ template/src/store/store.js
@@ -1,6 +1,13 @@
 import {proxy, useSnapshot} from 'valtio';
 import initialState from './initialState';
+import {devtools} from 'valtio/utils';
+import SETTINGS from '../config/settings';
 
 export const store = proxy(initialState);
 export const useStore = () => useSnapshot(store);
+
+if (SETTINGS.app.ENV === 'DEV') {
+  devtools(store, 'STORE');
+}
+
 export default store;
