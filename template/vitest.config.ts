006d1badbcdf3d41827bfd0032cc9b9fdb00b935
Gerson <gersondinis@gmail.com>
2023-01-15 15:01:25 -0100

    Finished vitest config

--- template/vitest.config.ts
+++ template/vitest.config.ts
@@ -1,17 +1,19 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
 
 export default defineConfig({
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   test: {
     root: 'src',
     globals: true,
     environment: 'jsdom',
-    // setupFiles: './src/setupTests.ts',
     coverage: {
       reporter: ['text', 'html'],
       exclude: [
         'node_modules/',
-        // 'src/setupTests.ts',
       ],
     },
   }
